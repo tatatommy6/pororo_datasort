15babe6663b59fdd0dd9ca854a5a7f4232fe9cb3
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-05-08 20:23:39 +0900

    Create 1.wav

--- src/sort_main.py
+++ src/sort_main.py
@@ -3,14 +3,14 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-HUGGINGFACE_API_KEY= os.getenv("HUGGINGFACE_API_KEY")
+# HUGGINGFACE_API_KEY= os.getenv("HUGGINGFACE_API_KEY")
 
 
 from pyannote.audio import Pipeline
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1")
 
 # inference on the whole file
-pipeline("../data/1.wav")
+diarization = pipeline("data/1.wav")
 
 # inference on an excerpt
 from pyannote.core import Segment
@@ -19,3 +19,4 @@ excerpt = Segment(start=2.0, end=5.0)
 from pyannote.audio import Audio
 waveform, sample_rate = Audio().crop("1_output.wav", excerpt)
 pipeline({"waveform": waveform, "sample_rate": sample_rate})
+print(diarization)
