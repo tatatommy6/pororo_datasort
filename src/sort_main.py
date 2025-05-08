f58333043b4b9b5517773f06c8a3ac30a4edc61e
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-05-08 19:30:06 +0900

    Update sort_main.py

--- src/sort_main.py
+++ src/sort_main.py
@@ -1,4 +1,3 @@
-from pyannote.audio import Pipeline
 import torch
 from dotenv import load_dotenv
 import os
@@ -7,18 +6,16 @@ load_dotenv()
 HUGGINGFACE_API_KEY= os.getenv("HUGGINGFACE_API_KEY")
 
 
-audio_file = "data/1.wav"
-use_auth_token = "HUGGINGFACE_API_KEY"  # 실제 Hugging Face 토큰으로 교체하세요
+from pyannote.audio import Pipeline
+pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1")
 
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-3.1",
-    use_auth_token=use_auth_token
-)
+# inference on the whole file
+pipeline("../data/1.wav")
 
-print(f"pipeline: {pipeline}")  # pipeline이 None인지 확인
+# inference on an excerpt
+from pyannote.core import Segment
+excerpt = Segment(start=2.0, end=5.0)
 
-if pipeline is None:
-    raise RuntimeError("Pipeline 로딩에 실패했습니다. Hugging Face 토큰을 확인하세요.")
-
-pipeline.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-diarization = pipeline(audio_file, num_speakers=2)
+from pyannote.audio import Audio
+waveform, sample_rate = Audio().crop("1_output.wav", excerpt)
+pipeline({"waveform": waveform, "sample_rate": sample_rate})
