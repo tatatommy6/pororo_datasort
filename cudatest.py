0127e3626d0b0fc9f785880781905647144d8726
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-07-13 17:33:17 +0900

    Changes to be committed: 쿠다 테스트 예외 대비 주석문 	modified:   cudatest.py

--- cudatest.py
+++ cudatest.py
@@ -4,6 +4,6 @@ print("✅ CUDA 사용 가능:", torch.cuda.is_available())
 print("🔢 CUDA 장치 수:", torch.cuda.device_count())
 if torch.cuda.is_available():
     print("🖥️ CUDA 장치 이름:", torch.cuda.get_device_name(0))
-    print("🧱 PyTorch 빌드된 CUDA 버전:", torch.version.cuda)
+    print("🧱 PyTorch 빌드된 CUDA 버전:", torch.version.cuda) # type: ignore
 else:
     print("⚠️ CUDA를 인식하지 못했습니다.")
